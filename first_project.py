9ef6daf2a72cdb477dc3ba009d78a3bbfcd824b9
HugoRoca <hugo.rock20@hotmail.com>
2022-09-14 23:35:10 -0500

    first steps in python

---
+++ first_project.py
@@ -0,0 +1,6 @@
+print("hello word")
+print(123+123)
+print("Esta linea\n\tva saltar")
+print("Barra Normal: /\nBarra Invertida: \\")
+
+print("Your name is: " + input("Say me your name: ") + " " + input("Say me your last name: "))
---
+++ random_name.py
@@ -0,0 +1,3 @@
+word_1 = input("Enter first word: ")
+word_2 = input("Enter second word: ")
+print("Cerveza's name\nis '" + word_1 + " " + word_2 + "'\nEnd!")
