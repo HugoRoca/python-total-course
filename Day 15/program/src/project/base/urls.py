bffb49ccb3ffda180569bfc607881b288284ecc1
HugoRoca <hugo.rock20@hotmail.com>
2022-11-19 18:23:26 -0500

    Day 15 - CRUD

--- Day 15/program/src/project/base/urls.py
+++ Day 15/program/src/project/base/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import PendingList, DetailTask
+from .views import PendingList, DetailTask, CreateTask, UpdateTask, DeleteTask
 
-urlpatterns = [path('', PendingList.as_view(), name='pendings'),
-               path('task/<int:pk>', DetailTask.as_view(), name='task')]
+urlpatterns = [path('', PendingList.as_view(), name='tasks'),
+               path('task/<int:pk>', DetailTask.as_view(), name='task'),
+               path('create-task', CreateTask.as_view(), name='create-task'),
+               path('edit-task/<int:pk>', UpdateTask.as_view(), name='edit-task'),
+               path('delete-task/<int:pk>', DeleteTask.as_view(), name='delete-task')]
