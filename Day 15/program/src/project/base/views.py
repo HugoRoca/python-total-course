c4694307ba7a19b8d7f760e5140b2d2bfdcab754
HugoRoca <hugo.rock20@hotmail.com>
2022-11-21 09:02:13 -0500

    Day 15 login

--- Day 15/program/src/project/base/views.py
+++ Day 15/program/src/project/base/views.py
@@ -2,35 +2,46 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Task
 
 
-class PendingList(ListView):
+class Login(LoginView):
+    template_name = 'base/login.html'
+    field = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+
+class PendingList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
 
-class DetailTask(DetailView):
+class DetailTask(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     # in case change the name of template
     # template_name = 'base/task.html'
 
 
-class CreateTask(CreateView):
+class CreateTask(LoginRequiredMixin, CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
 
-class UpdateTask(UpdateView):
+class UpdateTask(LoginRequiredMixin, UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
 
-class DeleteTask(DeleteView):
+class DeleteTask(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
